ce21a4e92cc8b5b8d6d6457f5277ce9393a23e4e
Rajesh <rajkishoresatapathy2002@gmail.com>
2026-09-14 01:23:07 +0530

    changes in the AddTodo placholder

--- frontend/src/components/AddTodo.jsx
+++ frontend/src/components/AddTodo.jsx
@@ -16,7 +16,7 @@ function AddTodo({ onAdd }) {
         type="text" 
         value={title} 
         onChange={(e) => setTitle(e.target.value)} 
-        placeholder="Add sf a new task" 
+        placeholder="Add sif a new task" 
       />
       <button type="submit">Add</button>
     </form>
